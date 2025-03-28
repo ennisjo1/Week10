8b4ae89ed6b11f360df7e2662b999c67e8b63d84
ennisjo <ennisjo1@msu.edu>
2025-03-28 00:51:15 +0000

    Working as in Videos

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,71 @@ import './App.css'
 
 function MagicButton() {
   return(
-    <button>Magic </button>
+    <div>
+    <h3>This is a Magic Button</h3>
+    <button> Magic </button>
+    </div>
   );
 }
-function App() {
-  const [count, setCount] = useState(0)
 
+const book = {
+  title: 'A Farewell to Arms',
+  author: 'Earnest Hemingway',
+  published: '1929',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/2/29/Book_cover_photo_for_%22Family_and_the_State_in_Soviet_Lithuania%22.jpg',
+  width: '264',
+  height: '378'
+};
+
+const magazines = [
+  { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
+  { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
+  { id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false },
+];
+
+function ZineRack() {
+  const listZines = magazines.map(zine =>
+    <li
+    key={zine.id}
+    style={{
+      color: zine.isAvailable ? 'green' : 'red'
+    }}
+  >
+    {zine.title}
+  </li>
+  );
+  return(
+    <ul> {listZines} </ul>
+  )}
+function Bookshelf() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      {book.image &&
+      <img
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      />
+      }
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <div className="App">
+      <h1>Week 10.3</h1>
+      <header className="App-header">
+      <ZineRack/>
+      <Bookshelf/>
+      </header>
+    </div>
+  );
+}
+export default App;
